e4a11a4dbc5485f3f35e2bff01123a8750230e42
Jackie Lealess <jlealess@gmail.com>
2018-11-01 20:39:06 -0400

    Adding form submit

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ class App extends Component {
   // uncomment this to test that you are connected to the backend!
   componentDidMount() {
     pollGroup();
-    submitPollQuestion();
   }
   render() {
     return (
