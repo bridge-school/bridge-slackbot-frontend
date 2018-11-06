8c5c92681222f56a5690918d43250220f4f86e4a
Jackie Lealess <jlealess@gmail.com>
2018-11-05 21:05:51 -0500

    Adds router and stub for Poll component

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,8 @@
 export const BRIDGEBOT_ACTIONS = {
+<<<<<<< HEAD
   GET_POLL_QUESTIONS: "bridgebot/GET_POLL_QUESTIONS",
+=======
+>>>>>>> Adds router and stub for Poll component
   RESET_FORM: "bridgebot/RESET_FORM",
   SAVE_POLL_GROUPS: "bridgebot/SAVE_POLL_GROUPS",
   SET_POLL_GROUP: "bridgebot/SET_POLL_GROUP",
